5e043faf2486eaf4350b6ad77d87e728811bd34d
An Tran Minh <tranminhan.my@gmail.com>
2015-12-09 01:39:14 +0700

    basic shout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @shouts = @user.shouts
+  end
+
+  def create
+    @user = sign_up(user_params)
+
+    if @user.valid?
+      sign_in(@user)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
+
